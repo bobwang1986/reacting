a1b99486a1a825495bb2831cff846f549f990f92
xinfeng3031 <xinfeng3031@163.com>
2017-11-29 14:34:22 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    user: 'http://ai-gogo.s1.natapp.cc//user',//'http://localhost:3000',
-    votes:'http://ai-gogo.s1.natapp.cc//vote'//'http://localhost:3001',
+    user: 'http://ai-gogo.s1.natapp.cc/user',//'http://localhost:3000',
+    votes:'http://ai-gogo.s1.natapp.cc/vote'//'http://localhost:3001',
 }
 
 export default config
