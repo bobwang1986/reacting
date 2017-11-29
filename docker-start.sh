61b839af0b7708fe3656a453baa3e8d1e83fb4db
payton <18991922353@163.com>
2017-11-29 16:54:47 +0800

    add -d param

--- docker-start.sh
+++ docker-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name vote_system_admin_ui --link="minioa-mongo:mongodb" vote_system_admin_ui:1.0
+docker run --name vote_system_admin_ui --link="minioa-mongo:mongodb" -d vote_system_admin_ui:1.0
